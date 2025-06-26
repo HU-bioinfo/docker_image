0bf8e57ddb5a8d439376afbcf4c6899d6639420f
kohta-hatanaka <htnk.duckbat6412@gmail.com>
2025-06-26 17:59:35 +0900

    del: base_env/hu_bioinfo.Dockerfile and scripts/

--- base_env/scripts/install_py.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -qO- https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="/usr/local/bin" sh
-
-mkdir -p -m 777 $UV_PYTHON_INSTALL_DIR
-uv python install $PYTHON_VERSION --preview --default
-uv python install 3.12.10
